4b469f02d5c144e72e740561edd0467bc1d308da
Nargiza Dzh <nargizadzh@gmail.com>
2023-10-30 00:58:13 +0600

    feat: solving problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,35 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.range = [];
+    }
+  
     setRange(min, max) {
-
+      this.range = [min, max];
     }
-
+  
     guess() {
-
+      const [min, max] = this.range;
+      if (min === max) {
+        return min;
+      } else if (min > max) {
+        return -1;
+      } else {
+        return Math.ceil((min + max) / 2);
+      }
     }
-
+  
     lower() {
-
+      const [min, max] = this.range;
+      let mid = Math.ceil((min + max) / 2);
+      this.range = [min, mid];
     }
-
+  
     greater() {
-
+      const [min, max] = this.range;
+      let mid = Math.ceil((min + max) / 2);
+      this.range = [mid, max];
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
+  
